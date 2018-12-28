8773e9fd939878348f7897499cd6196111c06ae0
harpiechoise <jcrispis56@gmail.com>
2018-12-28 17:04:12 -0300

    Comentarios Agregados - Sistema de validacion de tokens

--- RedSocial/app.py
+++ RedSocial/app.py
@@ -1,100 +1,103 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_mail import Mail, Message
 import os
-from dbUtils import register as registrarDB
+from dbUtils import register as registrarDB #Obtenemos el archivo register desde dbUtils
+from dbUtils import tokens as tokenDB #Obtenemos el archivo tonke ns desde dbUtils
+from flask import jsonify
 
-mailServer = Mail()
-app = Flask(__name__)
-app.config.update(dict(
-    DEBUG = True,
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = 587,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
-    MAIL_USERNAME = 'mail',
-    MAIL_PASSWORD = 'contraseña',
+mailServer = Mail() #Creamos un servidor de Email
+app = Flask(__name__) #Creamos la app de flask
+app.config.update(dict(                     #
+    DEBUG = True,                           #
+    MAIL_SERVER = 'smtp.gmail.com',         #
+    MAIL_PORT = 587,                        # Configuramos la smtp gratis de gmail
+    MAIL_USE_TLS = True,                    #   
+    MAIL_USE_SSL = False,                   #
+    MAIL_USERNAME = 'Email',                #
+    MAIL_PASSWORD = 'Contraseña',           #
 ))
-mailServer.init_app(app)
-app.secret_key = os.urandom(32)
-@app.route('/', methods=["GET", "POST"])
+mailServer.init_app(app) #Iniciamos el servidor de correos
+app.secret_key = os.urandom(32) #Ponemos la secret key encriptar las sessions y cookies
+@app.route('/', methods=["GET", "POST"]) #Configuramos la ruta de indice
 def index():
-    if request.method == "POST":
-        session['mail'] = request.form.get('email')
-        return redirect(url_for('login'))
+    if request.method == "POST": #Si el usuario apreta el boton del formulario
+        session['mail'] = request.form.get('email') #Obtenemos el valor del campo con nombre emal
+        #TODO: comparacion de contraseñas
+        return redirect(url_for('login')) #Redireccionamos al metodo login que esta mas abajo
     
-    return render_template('index.html')
+    return render_template('index.html') #Cuando el metodo sea GET renderizamos la template index.html
 
-@app.route('/register', methods=["GET", "POST"])
-def register():
-    mail = request.form.get('email')
-    password = request.form.get('pass')
-    confirm_pass = request.form.get('pass2')
-    if request.method == "POST":
-        if not password == confirm_pass:
-            password_error = 'Las contaseñas no coinciden'
-            flash(password_error)
-        else:
-           user_id = registrarDB.registrar_usuario(mail, password)
-           if not user_id:
-               pass
-           else: 
-            print(user_id)
-            link = "http://127.0.0.1:5000/confirm_step/{}".format(user_id)
-            print(mail)
+@app.route('/register', methods=["GET", "POST"]) #Configuramos la ruta register
+def register(): 
+    if request.method == "POST": #Si se presiona el boton del formulario
+        mail = request.form.get('email') #Se obtiene el valor del campo con nombre email
+        password = request.form.get('pass') #Se obtiene el valor del campo con nombre pass
+        confirm_pass = request.form.get('pass2') # Se obtiene el valor del campo pass2
+        if not password == confirm_pass: # Si las contraseñas no coinciden
+            password_error = 'Las contaseñas no coinciden' #Creamos un mensaje que diga las contraseñas no coinciden
+            flash(password_error) #Mandamos el mensaje al HTML con el metodo "flash"
+        else: #Si las contraseñas coinciden
+           user_id = registrarDB.registrar_usuario(mail, password) # Registramos el usuario con la funcion registrar usuario 
+           if not user_id: #Si el usuario ya existe
+               user_exist = "El usuario ya existe"  #
+               flash(user_exist)                    #Enviamos un mensaje al HTML con el metodo "flash"
+           else: #Si el usuario no existe
+            token = tokenDB.createToken(user_id) #Creamos el token en la base de datos
+            link = "http://127.0.0.1:5000/confirm_step/{}".format(token) #Formateamos la string con el token
+            #Creamos el mensaje de confirmacion
+            msg = Message("Confirma tu mail", sender='jcrispis56@gmail.com', recipients=[mail], body="Hola {} te damos la bienvenida a la pagina para confirmar sigue este link: \n\n{} \n\n este link tiene una duracion de 30 minutos".format(mail, link))
+            mailServer.send(msg) #Enviamos el mail
+            session['register'] = True #Creamos una cookie para decir que el usuario esta registrado
+            return redirect(url_for('confirm')) #Redireccionamos a la pagina de confirmacion 
+    return render_template('register.html') # Si el metodo es get retornamos la template
 
-            msg = Message("Confirma tu mail", sender='jcrispis56@gmail.com', recipients=[mail], body="Hola {} te damos la bienvenida a la pagina para confirmar sigue este link: \n{}".format(mail, link))
-            mailServer.send(msg)
-            session['register'] = True
-            return redirect(url_for('confirm'))
-    return render_template('register.html')
-
-@app.route('/confirm', methods=["GET"])
-def confirm():
-    if "register" in session:
-        session.pop('register', None)
-        return render_template('confirm.html')
+@app.route('/confirm', methods=["GET"]) #Configuramos la ruta
+def confirm(): 
+    if "register" in session: #Comprobamos si el usuario termino el registro
+        session.pop('register', None) #Quitamos la cookie de registro
+        return render_template('confirm.html') #Devolvemos el template de confirm
     else:
-        return "Algo ha salido mal :("
-@app.route('/confirm_step/<RegID>')
-def confirm_step(RegID):
+        return "Algo ha salido mal :(" #Si no devolvemos un error
+@app.route('/confirm_step/<RegID>') #Configuramos la ruta de confirmacion y como variable leemos el token
+def confirm_step(RegID): #Hacemos la funcion que se ejecuta al acceder a la ruta reciviendo como parametro el token
     RegID = str(RegID)
-
-    confirmacion = registrarDB.confirmar(RegID)
-    if not confirmacion:
-        return "Hubo un error en procesar los datos vuelva a intentarlo"
-    else:
-        session['Redirected'] = True
-        return redirect(url_for('gracias'))
-    return " "
-
+    confirmacion = tokenDB.getTokenAndConfirm(RegID) #Llamamos a la funcion de tokens.py para confirmar el usuario
+    if not confirmacion: #Si la funcion devuelve nulo o false
+        return "Hubo un error en procesar los datos vuelva a intentarlo" #Devolvemos una string al navegador
+    else: # Si la funcion devuelve True
+        session['Redirected'] = True #Ponemos la cookie de redirected
+        return redirect(url_for('gracias')) #Y redireccionamos a la pagina de agradecimiento
+    return " " #No retornamos nada
+ 
 @app.route('/gracias')
 def gracias():
-    if 'Redirected' in session:
-        session.pop("Redirected", None)
-        return render_template('gracias.html')
-    else: 
-        return "Algo ha salido mal :("
+    if 'Redirected' in session: #Si la cookie "Redirected" esta presente
+        session.pop("Redirected", None) #Quitamos la cookie
+        return render_template('gracias.html') #Enviamos el html al navegador
+    else: #Si npo esta la cookie
+        return "Algo ha salido mal :(" #Enviamos una string al navegador
 
-@app.route('/login')
+@app.route('/login') #Configuramos la ruta login
 def login():
-    if 'mail' in session:
-        mail = session.get('mail')
-        mongoResp = registrarDB.return_confirmed(mail)
-        if mongoResp[0] == True:
-            session.pop('mail', None)
-            return "bienvenido: "+mail
-        elif mongoResp[0] == False:
-            session.pop('mail', None)
-            session['register'] = True
+    if 'mail' in session: #Si la sesion contiene un mail
+        mail = session.get('mail') #Obtenemos el email de las cookies
+        mongoResp = registrarDB.return_confirmed(mail) #Comprobamos si esta confirmado
+        if mongoResp[0] == True: #Si esta confirmado
+            return "bienvenido: "+mail #Le damos una bienvenida
+            #TODO: Pagina de incio de usuario
+        elif mongoResp[0] == False: #Si la respuesta de mongo es falsa
+            session.pop('mail', None) # Quitamos el mail de las cookies
+            session['register'] = True # Ponemos la cooke para mostrar la pagina para confirmar mail
             return redirect(url_for('confirm'))
-        elif mongoResp[0] == 3:
-            session.pop('mail', None)
-            return redirect(url_for('register'))
-    else:
-        return "Algo salio mal :("
+        elif mongoResp[0] == 3: #Si el usuario no esta registrado
+            session.pop('mail', None) #Quitamos el mail de las cookies
+            #TODO: Autorellenado de formulario
+            return redirect(url_for('register')) #Y lo redireccionamos a la pagina de registro
+    else: #Si no tiene un mail
+        return "Algo salio mal :(" #Enviamos un string al navegador
 
         
     
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': #Si lo ejecutamos desde este archivo
+    app.run(debug=True) #Corremos la aplicacion en modo debug
     
--- RedSocial/dbUtils/register.py
+++ RedSocial/dbUtils/register.py
@@ -2,62 +2,71 @@ from pymongo import MongoClient
 from bson import ObjectId
 from bson.json_util import dumps
 from datetime import datetime
-
+import json
 def connection():
-    client = MongoClient()
-    db = client['red_social']
-    tabla = db['users']
-    return tabla
+    client = MongoClient() #Creamos el cliente de mongo
+    db = client['red_social'] #Entramos a la db Red_social
+    tabla = db['users'] #Entramos a la tabla de usuarios
+    return tabla #Devolvemos la tabla
+
 
 def registrar_usuario(mail, contraseña):
-    tabla = connection()
-    
-    existente = tabla.find_one({'mail':mail})
-    if existente:
-        print('El mail ya existe')
-        return None
-    
-    new_user = {}
-    new_user['date'] = datetime.utcnow()
-    new_user['mail'] = mail
-    new_user['contraseña'] = contraseña
-    new_user['confirm'] = False
-    user_id = tabla.insert(new_user) 
-    return user_id
+    tabla = connection() #Obtenemos la tabla de usuarios
+     
+    existente = tabla.find_one({'mail':mail}) #Buscamos si el mail esta registrado
+    if existente: #Si el valor no es None
+        return None #Retornamos None para parsearlo en el archivo app.py
+    new_user = {} #Si no creamos un diccionario para el usuario
+    new_user['date'] = datetime.now().timestamp() #Obtenemos la hora del registro
+    new_user['mail'] = mail #Guardamos el mail
+    #TODO: Encriptar contraseñas
+    new_user['contraseña'] = contraseña #Guardamos la contraseña
+    new_user['confirm'] = False #Guardamos la confirmacion por correo como falsa porque no se ha hecho
+    user_id = tabla.insert(new_user) #Insertamos el usuario y guardamos su ID
+    return user_id #Retornamos la ID para usarla despues
 
 def confirmar(ID):
-    tabla = connection()
-    id_modify = {"_id":ObjectId(ID)}
-    new_value = {'$set':{"confirm":True}}
-    count = tabla.update_one(id_modify, new_value)
-    print(count.modified_count, ' Valores modificados')
-    return True
-    
+    tabla = connection() #Obtenemos la tabla
+    id_modify = {"_id":ObjectId(ID)} #Ponemos la id de usuario para hacer la busqueda
+    new_value = {'$set':{"confirm":True}} #Ponemos el nuevo valor que seria confirmado
+    count = tabla.update_one(id_modify, new_value) #Actualizamos los valores de confirmacion
+    if count > 0: #Si se modificaron valores
+        return True #Retornamos que se modificaron los valores de la tabla
+    else:
+        return False #Retornamos que no se encontraron dichos valores
+#Funciones para testear
 def delete(ID):
-    tabla = connection()
-    tabla.delete_one({'_id':ObjectId(ID)})
-    print('Borrado')
-
+    tabla = connection() #Obtenemos la tabla 
+    tabla.delete_one({'_id':ObjectId(ID)}) #Borramos una ID
+    print('Borrado') #Mandamos un mensaje por consola
+#Funciones para testear
 def find_all():
-    tabla = connection()
-    vals = tabla.find()
-    print(dumps(vals))
-    
+    tabla = connection() #Obtenemos la tabla
+    vals = tabla.find() #Encontramos los valores
+    return dumps(vals) #Los pasamos de bson a texto
+#Funciones para testear
 def find_one(ID):
-    tabla = connection()
-    var = tabla.find_one({'_id':ObjectId(ID)})
-    return dumps(var)
+    tabla = connection() #Obtenemos la tabla
+    var = tabla.find_one({'_id':ObjectId(ID)}) #Encontramos el valor con dicha ID
+    return dumps(var) #Pasamos de BSON a texto
+#Funciones para testear
+def delete_all():
+    var = json.loads(find_all()) #Pasamos de texto a JSON
+    for i in var: #Iteramos el JSON
+        ID = i['_id']['$oid'] #Obtenemos la ID para borrar
+        delete(ID) #Borramos el valor
 
 def return_confirmed(correo):
-    tabla = connection()
-    var = tabla.find_one({'mail':correo})
+    tabla = connection() #Obtenemos la tabla
+    var = tabla.find_one({'mail':correo}) #Buscamos el valor con ese correo 
     try:
-        if var["confirm"] == True:
-            return (True, str(var["_id"]), var["mail"])
+        if var["confirm"] == True: #Intentamos buscar el valor
+            return (True, str(var["_id"]), var["mail"]) #Retornamos que el usuario esta confirmado
         elif var["confirm"] == False:
-            return (False, 0, 0)
+            return (False, 0, 0) #Retornamos que esta registrado pero no confirmado
     except:
-        return (3,0,0)
-if __name__ == '__main__':
-    find_all()
+        return (3,0,0) #Retornamos que no esta registrado
+if __name__ == '__main__': #Si se ejecuta desde este archivo
+    delete_all() #
+    find_all()   #
     
